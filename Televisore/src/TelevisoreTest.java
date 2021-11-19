a98ac7e72d6d4d5cd7df41cdbdff77df255e8033
RamondettiMarco <93180431+RamondettiMarco@users.noreply.github.com>
2021-11-19 10:23:48 +0100

    Update TelevisoreTest.java

--- Televisore/src/TelevisoreTest.java
+++ Televisore/src/TelevisoreTest.java
@@ -1,6 +1,9 @@
+import java.util.*;
+
 public class TelevisoreTest {
     public static void main(String[] args) {
         Televisore tv = new Televisore(20, "HD", "nero");
+        Scanner a = new Scanner(System.in);
         System.out.println("Informazioni generiche televisore:");
         System.out.println("Pollici: " +tv.getPollici());
         System.out.println("Schermo: " +tv.getSchermo());
@@ -10,7 +13,9 @@ public class TelevisoreTest {
         System.out.println("Luminosità:" +tv.getLuminosita());
         tv.aumentaLuminosita();
         System.out.println("Luminosità attuale: " +tv.getLuminosita());
-        tv.set_canale(5);
+        System.out.println("Inserisci canale: ");
+        int c = a.nextInt();
+        tv.set_canale(c);
         System.out.println("Canale: " +tv.getCanale());
         tv.canaleSuccessivo();
         System.out.println("Nuovo Canale: " +tv.getCanale());
@@ -20,6 +25,25 @@ public class TelevisoreTest {
         tv.spegni();
         System.out.println("Televisore acceso: " +tv.isAcceso());
 
+        //SECONDO OGGETTO, VALORI INSERITI DALL'UTENTE
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Secondo televisore: ");
+        Televisore tv2 = new Televisore(32, "AMOLED", "Bianco");
+        System.out.println("Inserisci canale: ");
+        int canale = a.nextInt();
+        tv2.set_canale(canale);
+        System.out.println("Inserisci luminosità: ");
+        int luminosita = a.nextInt();
+        tv2.set_Luminosita(luminosita);
+        System.out.println("Inserisci volume: ");
+        int volume = a.nextInt();
+        tv2.set_Volume(volume);
+        tv2.accendi();
 
+        if(tv2.equals(tv)){
+            System.out.println("L'oggetto1 e l'oggetto 2 Sono UGUALI");
+        }else{
+            System.out.println("L'oggetto1 e L'oggetto2 Sono DIVERSI");
+        }
     }
 }
