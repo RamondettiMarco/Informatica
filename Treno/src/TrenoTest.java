1cd53c1f32f89486093956584861c8835066343b
RamondettiMarco <93180431+RamondettiMarco@users.noreply.github.com>
2021-11-19 10:40:15 +0100

    Update TrenoTest.java

--- Treno/src/TrenoTest.java
+++ Treno/src/TrenoTest.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TrenoTest {
     public static void main(String[] args) {
         Treno t = new Treno("Trenitalia", "Prima", 220, "15.30", "16.45");
@@ -6,5 +8,21 @@ public class TrenoTest {
         System.out.println("Velocità: " +t.getVelocita() +"km/h");
         System.out.println("Partenza: " +t.getPartenza());
         System.out.println("Arrivo: " +t.getArrivo());
+
+        //SECONDO TRENO
+        System.out.println("-------------------------------------");
+        System.out.println("Secondo Treno: ");
+        Scanner a = new Scanner(System.in);
+        System.out.println("inserisci la velocita in km/h: ");
+        int v = a.nextInt();
+        Treno t2 = new Treno("Trenitalia", "Prima", v, "15.30", "16.45");
+
+        if(t2.equals(t)){
+            System.out.println("L'oggetto1 e l'oggetto 2 Sono UGUALI");
+        }else{
+            System.out.println("L'oggetto1 e L'oggetto2 Sono DIVERSI");
+
+
+        }
     }
 }
